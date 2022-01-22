e6e0b2f718d4395bbaa4914455065d0403532b15
OgatsuMayuko <95006358+OgatsuMayuko@users.noreply.github.com>
2022-01-22 14:42:00 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,17 +1,18 @@
 function tokei(){
   var d = new Date();
 
-  var h = d.getHours();
-  var m = d.getMinutes();
-  var s = d.getSeconds();
+  var h = d.getHours();//時間
+  var m = d.getMinutes();//分
+  var s = d.getSeconds();//秒
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
+  //表示の方法を書く
   nowtime = h + ":" + m + ":" + s;
   var t = document.getElementById('time');
-  
+  // 今の時間を表示する
  
   t.innerHTML = nowtime; 
 }
