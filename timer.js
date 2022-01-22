d06b5210f621bcc719ee9fdad63f87ca436aaac6
OgatsuMayuko <95006358+OgatsuMayuko@users.noreply.github.com>
2022-01-22 13:52:11 +0900

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,20 @@
+function clock(){
+  var d = new Date();
+
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  nowtime = h + ":" + m + ":" + s;
+  var t = document.getElementById('time');
+  
+  t.style.textAlign = "center";
+  t.innerHTML = nowtime;
+}
+
+setInterval('clock()', 1000);
+
